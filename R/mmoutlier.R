a4ed7f7d7856aa961bd876e31cb561580d1f049a
Dr Manoj Kuppusamy <48274053+drkmanoj@users.noreply.github.com>
2021-09-30 15:47:46 +0530

    Add files via upload

---
+++ R/mmoutlier.R
@@ -0,0 +1,35 @@
+#'Identifying outliers from univariate data and replace the outliers by max-min Method'
+#'@export
+#'@param x numeric variable
+mmoutlier<-function(x)
+{
+  par(mfrow=c(1,2))
+x<-rnorm(1500)
+xsort<-sort(x)
+y<-boxplot(x)$out
+xy<-x[which(x %in% y)]
+xy
+
+xysort<-sort(xy)
+
+l<-length(xysort)
+#len<-1
+for(k in seq_along(xysort))
+{
+for(i in seq_along(x))
+{
+
+  if(xysort[k]==x[i])
+  {
+
+  x[i]<-(xsort[length(x)-length(xysort)-l+1])-x[(l)]
+ # len<-len-1
+  l<-l-1
+  }
+}
+}
+print(x)
+z<-boxplot(x)
+}
+
+
